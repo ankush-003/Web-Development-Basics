726841cc2cab6b094e5f73ab82864822c4297769
ankush-003 <anshhv2003@gmail.com>
2022-10-12 20:04:16 +0530

    learning NodeJS

---
+++ Unit 4/add.js
@@ -0,0 +1,4 @@
+var module = require('./func1.js');
+console.log(module.addfunc(6,9));
+console.log(module.datefunc());
+console.log(module.samplemsg);
---
+++ Unit 4/func1.js
@@ -0,0 +1,9 @@
+exports.addfunc = function add(a,b) {
+    return a + b;
+}
+
+exports.datefunc = function() {
+    return Date();
+}
+
+exports.samplemsg = 'Hello PES!'
