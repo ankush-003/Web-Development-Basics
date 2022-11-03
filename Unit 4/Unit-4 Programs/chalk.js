35e3ee9b0fc94f8ada8d6a70e1999851491552a0
ankush-003 <anshhv2003@gmail.com>
2022-11-03 21:36:18 +0530

    updated chalk

--- Unit 4/Unit-4 Programs/chalk.js
+++ Unit 4/Unit-4 Programs/chalk.js
@@ -1,7 +1,7 @@
 
 //--------Chalk---------------
 
-const chalk = require('chalk');				//import chalk from 'chalk';
+const chalk = require('../Class/node_modules/chalk');				//import chalk from 'chalk';
 console.log(chalk.blue('Hello world!'));
 
 console.log(chalk.red("Hello chalk world ...."))
