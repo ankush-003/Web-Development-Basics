f51683d2274cfeb50853a53e0113f84d6cca147e
ankush-003 <anshhv2003@gmail.com>
2022-12-04 20:46:28 +0530

    revising events

--- Assignments/MongoDB/client.js
+++ Assignments/MongoDB/client.js
@@ -17,12 +17,30 @@ fetch('http://localhost:8081/api', {
         "compName": "IBM",
         "compRank": 2,
         "compWebsite": "www.ibm.com",
-        "compAddress": "Nagavara, Bangalore"},
+        "compAddress": "Nagavara, Bangalore",
+        "empCount": 10000,
+        "cmpRevenue": 1000000000},
         {
-        "compName": "Infosys",
-        "compRank": 3,
-        "compWebsite": "www.infosys.com",
-        "compAddress": "Bangalore",
+            "compName": "TCS",
+            "compRank": 3,
+            "compWebsite": "www.tcs.com",
+            "compAddress": "Nagavara, Bangalore",
+            "empCount": 10000,
+            "cmpRevenue": 1000000000
+        },{
+            "compName": "Wipro",
+            "compRank": 4,
+            "compWebsite": "www.wipro.com",
+            "compAddress": "Nagavara, Bangalore",
+            "empCount": 10000,
+            "cmpRevenue": 1000000000
+        },{
+            "compName": "Accenture",
+            "compRank": 5,
+            "compWebsite": "www.accenture.com",
+            "compAddress": "Nagavara, Bangalore",
+            "empCount": 10000,
+            "cmpRevenue": 1000000000
         }]),
     headers: { 'Content-Type': 'application/json' },
 })
