ea8e7d4a58ed7cddf875e50f9fbf929198e86b16
Ankush H V <anshhv2003@gmail.com>
2023-01-11 01:37:51 +0530

    learning javascript coursera

---
+++ Learning JavaScript/objects.js
@@ -0,0 +1,5 @@
+let person1 = {
+    "name": "Ankush"
+}
+
+console.log(person1.name)
---
+++ Learning JavaScript/var_scope.js
@@ -0,0 +1,12 @@
+if (true) {
+    var a = 10
+    let b = 20
+    console.log(a)
+}
+console.log(a)
+try {
+    console.log(b)
+}
+catch (e) {
+    console.log(e.message)
+}
